faf78394919d4b6a1bc1c5e11d026b791deb5167
Aman Dubey <amandubey918@gmail.com>
2022-09-28 20:22:40 +0530

    Made layout of the home page, added the search bar

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,12 +1,12 @@
 import { Food } from './app/shared/models/Food';
-import { Tag } from './';
+//import { Tag } from './';
 
 export const sample_foods: Food[] = [
   {
     id: '1',
     name: 'Pizza Pepperoni',
     cookTime: '10-20',
-    price: 10,
+    price: 100,
     favorite: false,
     origins: ['italy'],
     stars: 4.5,
@@ -16,7 +16,7 @@ export const sample_foods: Food[] = [
   {
     id: '2',
     name: 'Meatball',
-    price: 20,
+    price: 200,
     cookTime: '20-30',
     favorite: true,
     origins: ['persia', 'middle east', 'china'],
@@ -27,7 +27,7 @@ export const sample_foods: Food[] = [
   {
     id: '3',
     name: 'Hamburger',
-    price: 5,
+    price: 500,
     cookTime: '10-15',
     favorite: false,
     origins: ['germany', 'us'],
@@ -38,7 +38,7 @@ export const sample_foods: Food[] = [
   {
     id: '4',
     name: 'Fried Potatoes',
-    price: 2,
+    price: 200,
     cookTime: '15-20',
     favorite: true,
     origins: ['belgium', 'france'],
@@ -49,7 +49,7 @@ export const sample_foods: Food[] = [
   {
     id: '5',
     name: 'Chicken Soup',
-    price: 11,
+    price: 110,
     cookTime: '40-50',
     favorite: false,
     origins: ['india', 'asia'],
@@ -60,7 +60,7 @@ export const sample_foods: Food[] = [
   {
     id: '6',
     name: 'Vegetables Pizza',
-    price: 9,
+    price: 400,
     cookTime: '40-50',
     favorite: false,
     origins: ['italy'],
@@ -70,13 +70,13 @@ export const sample_foods: Food[] = [
   },
 ];
 
-export const sample_tags: Tag[] = [
-  { name: 'All', count: 6 },
-  { name: 'FastFood', count: 4 },
-  { name: 'Pizza', count: 2 },
-  { name: 'Lunch', count: 3 },
-  { name: 'SlowFood', count: 2 },
-  { name: 'Hamburger', count: 1 },
-  { name: 'Fry', count: 1 },
-  { name: 'Soup', count: 1 },
-];
+// export const sample_tags: Tag[] = [
+//   { name: 'All', count: 6 },
+//   { name: 'FastFood', count: 4 },
+//   { name: 'Pizza', count: 2 },
+//   { name: 'Lunch', count: 3 },
+//   { name: 'SlowFood', count: 2 },
+//   { name: 'Hamburger', count: 1 },
+//   { name: 'Fry', count: 1 },
+//   { name: 'Soup', count: 1 },
+// ];
