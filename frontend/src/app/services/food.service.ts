70f7605e1babe752f36d79c308a8a6fe13bd737d
Aman Dubey <amandubey918@gmail.com>
2022-09-28 16:26:09 +0530

    Added home component

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { sample_foods } from 'src/data';
+import { Food } from '../shared/models/Food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
-
-  constructor() { }
+  constructor() {}
+  getAll(): Food[] {
+    return sample_foods;
+  }
 }
